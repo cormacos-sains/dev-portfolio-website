b1041c6afedab1fd854277fb7d6f85ea3d688374
Cormac O'Sullivan <cormac.osullivan@sainsburys.co.uk>
2024-11-29 11:15:06 +0000

    fix typos

--- components/About.tsx
+++ components/About.tsx
@@ -19,7 +19,7 @@ export const About = () => {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with master's degree in{" "}
+        After graduating with master&apos;s degree in{" "}
         <span className="font-medium">Computer Science (MSc)</span>, I decided
         to pursue my passion for programming.{" "}
         <span className="italic">My favorite part of programming</span> is the
@@ -37,12 +37,12 @@ export const About = () => {
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="italic">When I&apos;m not coding</span>, I enjoy
+        playing video games, watching movies, and playing with my dog. I also
+        enjoy <span className="font-medium">learning new things</span>. I am
+        currently learning about{" "}
+        <span className="font-medium">history and philosophy</span>. I&apos;m
+        also learning how to play the guitar.
       </p>
     </motion.section>
   );
